d1c0d86b84a1adb6ec49c79bd012fc49617b21e6
xde013 <rbourhil@wiser.com>
2025-12-12 17:09:42 +0100

    use fastify and add swagger module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get hello corum' })
+  @ApiResponse({ status: 200, description: 'Returns a hello corum' })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Corum FS API')
+    .setDescription('The Corum FS API documentation')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
+
 bootstrap();
